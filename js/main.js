22b0cd26bbbb3b777f5dee43cc1c24856e632689
Josh Darling <squiddarling@gmail.com>
2023-02-11 01:24:25 -0500

    added additional CSS and updated html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-const header = document.getElementById("header");
+// const header = document.getElementById("header");
 
-const bio = document.getElementById("bio");
+// const bio = document.getElementById("bio");
 
-const acting = document.getElementById("acting");
+// const acting = document.getElementById("acting");
 
-const writing = document.getElementById("writing");
+// const writing = document.getElementById("writing");
 
-const gallery = document.getElementById("gallery");
+// const gallery = document.getElementById("gallery");
 
-const contact = document.getElementById("contact");
+// const contact = document.getElementById("contact");
+
+const moarActing = document.getElementById("moarActing");
+const moarWriting = document.getElementById("moarWriting");
+const addtionalActing = document.getElementById("addtionalActing");
+const addtionalWriting = document.getElementById("addtionalWriting");
+const hideWriting = document.getElementById("hideWriting");
+const hideActing = document.getElementById("hideActing");
+
+moarActing.addEventListener("click", (event) => {
+  addtionalActing.removeAttribute("hidden");
+});
+
+moarWriting.addEventListener("click", (event) => {
+  addtionalWriting.removeAttribute("hidden");
+});
+
+hideActing.addEventListener("click", (event) => {
+  addtionalActing.setAttribute("hidden", "");
+});
+
+hideWriting.addEventListener("click", (event) => {
+  addtionalWriting.setAttribute("hidden", "");
+});
