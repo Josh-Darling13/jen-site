377d3e88899fb4452d256642f45a7b364d524ca3
Josh Darling <squiddarling@gmail.com>
2023-02-10 21:27:20 -0500

    build basic html bones

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const header = document.getElementById("header");
+
+const bio = document.getElementById("bio");
+
+const acting = document.getElementById("acting");
+
+const writing = document.getElementById("writing");
+
+const gallery = document.getElementById("gallery");
+
+const contact = document.getElementById("contact");
