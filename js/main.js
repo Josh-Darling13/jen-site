aab5406a07767aee4abfcd78bdd3bb00138fef5b
Josh Darling <squiddarling@gmail.com>
2023-02-14 18:11:48 -0500

    updated CSS for desktop

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@
 
 // const gallery = document.getElementById("gallery");
 
-// const contact = document.getElementById("contact");
+// const contact = document.getElementById("contact")
+
+// const picAndBio = document.getElementById('picAndBio');
+
+
 
 const moarActing = document.getElementById("moarActing");
 const moarWriting = document.getElementById("moarWriting");
@@ -16,6 +20,12 @@ const addtionalActing = document.getElementById("addtionalActing");
 const addtionalWriting = document.getElementById("addtionalWriting");
 const hideWriting = document.getElementById("hideWriting");
 const hideActing = document.getElementById("hideActing");
+
+const pic1 = document.getElementById('pic1');
+const pic2 = document.getElementById('pic2');
+const pic3 = document.getElementById('pic3');
+const pic4 = document.getElementById('pic4');
+
 
 moarActing.addEventListener("click", (event) => {
   addtionalActing.removeAttribute("hidden");
@@ -32,3 +42,48 @@ hideActing.addEventListener("click", (event) => {
 hideWriting.addEventListener("click", (event) => {
   addtionalWriting.setAttribute("hidden", "");
 });
+
+let testBool = true;
+function toggle() {
+  testBool = !testBool;
+  return testBool;
+  }
+
+pic1.addEventListener('click', (event)=>{
+  const showPic = toggle();
+  if(!showPic){
+    pic1.src = "./img/bo_big.png";
+  } else {
+    pic1.src = "./img/bo_little.png";
+  }
+});
+
+pic2.addEventListener('click', (event)=>{
+  const showPic = toggle();
+  if(!showPic){
+    pic2.src = "./img/ru_big.png";
+  } else {
+    pic2.src = "./img/ru_little.png";
+  }
+});
+
+pic3.addEventListener('click', (event)=>{
+  const showPic = toggle();
+  if(!showPic){
+    pic3.src = "./img/Ingrid_big.png";
+  } else {
+    pic3.src = "./img/Ingrid_little.png";
+  }
+});
+
+pic4.addEventListener('click', (event)=>{
+  const showPic = toggle();
+  if(!showPic){
+    pic4.src = "./img/baby_big.png";
+  } else {
+    pic4.src = "./img/baby_little.png";
+  }
+});
+
+
+
